1de936ec12f8fd1811c1449d15fb2a02f915c983
Chris Watson <chris.watson@7digital.com>
2019-08-12 11:55:53 +0100

    Make dotfile backing up and linking more resilient

--- setup.sh
+++ setup.sh
@@ -7,28 +7,27 @@ set -eo pipefail
 
 ########## Variables
 
-dir=~/dotfiles                    # dotfiles directory
-olddir=~/dotfiles_old             # old dotfiles backup directory
+here=~/dotfiles                    # dotfiles directory
+backup_dir=~/dotfiles_old             # old dotfiles backup directory
 files="bashrc vimrc vim zshrc xchat2 tmux.conf"    # list of files/folders to symlink in homedir
 
 ##########
 
 # create dotfiles_old in homedir
-echo -n "Creating $olddir for backup of any existing dotfiles in ~ ..."
-mkdir -p $olddir
+echo -n "Creating $backup_dir for backup of any existing dotfiles in ~ ..."
+mkdir -p $backup_dir
 echo "done"
 
 # change to the dotfiles directory
-echo -n "Changing to the $dir directory ..."
-cd $dir
+echo -n "Changing to the $here directory ..."
+cd $here
 echo "done"
 
-# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks from the homedir to any files in the ~/dotfiles directory specified in $files
+echo "Backing up existing dotfiles to $backup_dir"
 for file in $files; do
-    echo "Moving any existing dotfiles from ~ to $olddir"
-    mv ~/.$file ~/dotfiles_old/
-    echo "Creating symlink to $file in home directory."
-    ln -s $dir/$file ~/.$file
+    [ -e "$file" ] && [ -e "~/.$file" ] && echo "Moving ~/.$file to $backup_dir." && mv -r "~/.$file" "$backup_dir"
+    echo "Symlinking "$here/$file" to ~/.$file"
+    ln -srf "$file" "$HOME/.$file" # ln doesn't understand ~ when quoted
 done
 
 mkdir -p /tmp/dotfiles
